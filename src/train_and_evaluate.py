0b49b88c554798791304a69afee2e7e7afaf8355
Chirag Agrawal <chirag.ism@gmail.com>
2022-03-26 15:30:13 -0400

    train and evaluate python file

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -66,7 +66,7 @@ def train_and_evaluate(config_path):
             "alpha": alpha,
             "l1_ratio" : l1_ratio
         }
-        json.dump(scores, f, indent=4)
+        json.dump(params, f, indent=4)
 
     os.makedirs(model_dir, exist_ok=True)
     model_path = os.path.join(model_dir, "model.joblib")
